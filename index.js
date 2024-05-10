5f8a7d2d7087154b37f0ca272330e921d2160e77
MessiasGui <119057255+MessiasGui@users.noreply.github.com>
2024-05-10 16:09:40 -0300

    Mudancas nos arquivos html e index.js
    Fiz mudancas para que as paginas html aparecam de forma correta com o css e o javascript no servidor do localhost pelo node.

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import express from "express";
+import path from "path";
+
 const app = express();
 const port = 3000;
+const __dirname = path.resolve();
 
-app.get("/", (req, res) => {
-    console.log(req.rawHeaders);
-    res.send("hello")
-})
+// Define o middleware para servir os arquivos estáticos do diretório 'pages'
+app.use(express.static(__dirname + '/pages'));
+app.use(express.static(__dirname + '/dist'));
 
-app.get("/home", (req, res) => {
-    console.log(req.rawHeaders);
-    res.send("hello")
-})
+// Rota para a página principal
+app.get('/', (req, res) => {
+  res.sendFile('welcome-page.html', { root: __dirname + '/pages'});
+});
 
 app.listen(port, () => {
     console.log('Server started on port ' + port);
